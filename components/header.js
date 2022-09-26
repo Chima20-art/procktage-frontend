4663d072ede84872a3c6cec057851ede1c11b37f
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-26 23:00:28 +0100

    created Header.js.

---
+++ components/header.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Header() {
+    return <div class="h-20 bg-indigo-500  "></div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/header'
 import styles from '../styles/Home.module.css'
+import { createClient } from 'next-sanity'
 
 export default function Home() {
-    return <div>hfvjdv</div>
+    return (
+        <div>
+            <Header />
+        </div>
+    )
 }

1529658fe9ccfd267c569da45ee53f007dd374d4
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-28 17:49:04 +0100

    imported colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                red: '#B0161E',
+                red: {
+                    50: '#fef2f3',
+                    100: '#ffe1e3',
+                    200: '#ffc8cb',
+                    300: '#ffa2a7',
+                    400: '#fd6c74',
+                    500: '#f53e49',
+                    600: '#e2202b',
+                    700: '#b0151e',
+                    800: '#9d171f',
+                    900: '#821a20',
+                },
                 gray: ' #424242',
             },
             fontFamily: {

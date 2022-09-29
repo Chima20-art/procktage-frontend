1a819a314d8c6f381cc17a66fd8265a07ad15f63
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-29 12:28:15 +0100

    added contact section.

--- pages/footer.js
+++ pages/footer.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { AiFillFacebook } from 'react-icons/ai'
 import { BsInstagram } from 'react-icons/bs'
 import { TbBrandTiktok } from 'react-icons/tb'
+import { ImLocation } from 'react-icons/im'
+import { BsTelephoneFill } from 'react-icons/bs'
+import { FaFax } from 'react-icons/fa'
+import { GoDeviceMobile } from 'react-icons/go'
+
+import { MdOutlineEmail } from 'react-icons/md'
 
 export default function Footer() {
     return (
         <div className="h-[350px] bg-[#FFF8ED] flex flex-col self-end w-full-screen">
-            <section className=" bg-red-700 py-4   ">
+            <section className="  py-4   text-[#424242] bg-red-700 ">
                 <div className="max-w-5xl  mx-auto  flex justify-between">
                     <p className="text-sm text-[#FEDFB6]">
                         Connectez-vous avec nous sur les réseaux sociaux :
@@ -18,24 +24,70 @@ export default function Footer() {
                     </div>
                 </div>
             </section>
-            <section className="flex flex-rw max-w-5xl py-6 mx-auto bg-red-300 w-screen">
-                <div className="bg-red-400   px-6  ">
-                    <p className=" uppercase bg-red-200 py-2 text-xs border-b-2 border-red-700 border-0">
+            <section
+                id="footer search"
+                className="flex flex-rw max-w-5xl py-6 mx-auto  w-screen"
+            >
+                <section className="   px-6 text-[#424242] ">
+                    <p className=" w-[204px] uppercase  py-2 text-xs border-b-2 border-red-700 border-0">
                         Recherche un produit
                     </p>
-                    <p className="text-[11px] uppercase py-2 flex justify-center text-[#555]">
+                    <p className="text-[9px] uppercase py-2 flex justify-center text-[#555]">
                         {' '}
                         taper le nom d'article, reference.
                     </p>
                     <input
                         type="text"
-                        className=" rounded-[50px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="shadow-2xl rounded-[50px] my-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5"
                         placeholder="Recherche un produit"
                         required
                     />
-                </div>
-                <div className="flex-1">produit</div>
-                <div className="flex-1">produit</div>
+                    <button className=" shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] rounded-[50px] bg-red-700 text-[#F5F5F5] text-sm rounded-lg focus:ring-red-700 focus:border-red-500 block w-full p-2.5">
+                        Recherche
+                    </button>
+                </section>
+                <section
+                    className="
+                 px-6 flex flex-col justify-between text-[#424242]"
+                >
+                    <p className=" w-[204px]  uppercase  py-2 text-xs border-b-2 border-red-700 border-0">
+                        Nos produit
+                    </p>
+                    <p className="uppercase text-[#424242]  text-xs  w-[204px]">
+                        emballage alimentaire pour la restauration
+                    </p>
+                    <p className="uppercase text-[#424242] text-xs  w-[204px]">
+                        emballage recyclable biodégradable
+                    </p>
+                    <p className="uppercase text-[#424242] text-xs  w-[204px]">
+                        le professionnel de l'hygiène
+                    </p>
+                </section>
+                <section className="w-full px-6 uppercase text-[#424242] text-xs ">
+                    <p className="   uppercase  py-2 text-xs  border-b-2 border-red-700 border-0">
+                        Contact
+                    </p>
+                    <div className="flex items-center pt-4 pb-1">
+                        <ImLocation />
+                        104 bis rue mimousa hay erraha casablanca - maroc
+                    </div>
+                    <div className="flex items-center py-1">
+                        <BsTelephoneFill />
+                        +212(0)522 95 18 54/522 36 52 72
+                    </div>
+                    <div className="flex items-center py-1">
+                        <GoDeviceMobile />
+                        +212(0)661 88 56 19
+                    </div>
+                    <div className="flex items-center py-1">
+                        <FaFax />
+                        +212(0)522 36 52 72
+                    </div>
+                    <div className="flex items-center py-1">
+                        <MdOutlineEmail />
+                        proxymarket@menara.ma - contact@proxymarketmaroc.com
+                    </div>
+                </section>
             </section>
         </div>
     )

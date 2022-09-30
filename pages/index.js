f574d97d7ed7d4797d1105cea62a7050922f3ff5
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-30 16:06:07 +0100

    changed bg-color.

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,12 @@ import Footer from './footer'
 
 export default function Home({ websiteSettings }) {
     return (
-        <div className="h-screen  ">
+        <div className="h-screen bg-[#FFF8ED] flex flex-col justify-between ">
             <Header websiteSettings={websiteSettings} />
-            <div className="h-1/3 ">Content</div>
-            <Footer websiteSettings={websiteSettings} />
+            <div className=" ">Content</div>
+            <div className="w-screen">
+                <Footer websiteSettings={websiteSettings} />
+            </div>
         </div>
     )
 }

20e47d052ccce2bf692a4262563d1f120933d504
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-01 12:36:57 +0100

    footer mediaQueries.

--- pages/footer.js
+++ pages/footer.js
@@ -33,10 +33,10 @@ export default function Footer({ websiteSettings }) {
             </section>
             <section
                 id="footer search"
-                className="flex flex-rw max-w-5xl py-6 mx-auto  w-screen"
+                className=" bg-red-100 flex-col flex px-4 md:px-0 md:py-2 md:flex-row md:flex md:justify-between max-w-5xl md:py-6 mx-auto  w-screen"
             >
-                <section className="   px-6 text-[#424242] ">
-                    <p className=" w-[204px] uppercase  py-2 text-xs border-b-2 border-red-700 border-0">
+                <section className=" md:px-2  flex flex-col  px-6 text-[#424242] ">
+                    <p className="    pt-4 pb-2 my-2   md:w-[204px] md:py-2 uppercase  text-xs border-b-2 border-red-700 border-0">
                         Recherche un produit
                     </p>
                     <p className="text-[9px] uppercase py-2 flex justify-center text-[#555]">
@@ -45,52 +45,52 @@ export default function Footer({ websiteSettings }) {
                     </p>
                     <input
                         type="text"
-                        className="shadow-2xl rounded-[50px] my-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5"
+                        className="max-w-sm flex self-center text-center shadow-2xl rounded-[50px] my-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5"
                         placeholder="Recherche un produit"
                         required
                     />
-                    <button className=" shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] rounded-[50px] bg-red-700 text-[#F5F5F5] text-sm rounded-lg focus:ring-red-700 focus:border-red-500 block w-full p-2.5">
+                    <button className="max-w-sm grid self-center content-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] rounded-[50px] bg-red-700 text-[#F5F5F5] text-sm rounded-lg focus:ring-red-700 focus:border-red-500 block w-full p-2.5">
                         Recherche
                     </button>
                 </section>
                 <section
                     className="
-                 px-6 flex flex-col justify-between text-[#424242]"
+                     md:px-6 px-6 flex flex-col justify-between text-[#424242]"
                 >
-                    <p className=" w-[204px]  uppercase  py-2 text-xs border-b-2 border-red-700 border-0">
+                    <p className="pt-4 pb-2 my-4 md:my-2   md:w-[204px] md:py-2 uppercase  text-xs border-b-2 border-red-700 border-0">
                         Nos produit
                     </p>
-                    <p className="uppercase text-[#424242]  text-xs  w-[204px] cursor-pointer hover:font-bold">
+                    <p className="uppercase text-[#424242]  text-xs my-2   md:w-[204px] cursor-pointer hover:font-bold">
                         emballage alimentaire pour la restauration
                     </p>
-                    <p className="uppercase text-[#424242] text-xs  w-[204px] cursor-pointer hover:font-bold">
+                    <p className="uppercase text-[#424242] text-xs my-2   md:w-[204px] cursor-pointer hover:font-bold">
                         emballage recyclable biodégradable
                     </p>
-                    <p className="uppercase text-[#424242] text-xs  w-[204px] cursor-pointer hover:font-bold">
+                    <p className="uppercase text-[#424242] text-xs my-2   md:w-[204px] cursor-pointer hover:font-bold">
                         le professionnel de l'hygiène
                     </p>
                 </section>
-                <section className="w-full px-6 uppercase text-[#424242] text-xs ">
-                    <p className="   uppercase  py-2 text-xs  border-b-2 border-red-700 border-0">
+                <section className="  w-full px-6 uppercase text-[#424242] text-xs  ">
+                    <p className=" pt-4 pb-2 my-4 md:my-2   md:w-[204px] md:py-2 uppercase  text-xs border-b-2 border-red-700 border-0">
                         Contact
                     </p>
-                    <div className="flex items-center pt-4 pb-1">
+                    <div className="flex items-center md:pt-2 md:py-0 py-3 pt-0 ">
                         <ImLocation className="text-lg" />
                         {adress}
                     </div>
-                    <div className="flex items-center py-1">
+                    <div className="flex items-center md:pt-2 md:py-0 py-3 pt-0">
                         <BsTelephoneFill className="text-lg" />
                         {phone}
                     </div>
-                    <div className="flex items-center py-1">
+                    <div className="flex items-center  md:pt-2 md:py-0 py-3 pt-0">
                         <GoDeviceMobile className="text-lg" />
                         {mobile}
                     </div>
-                    <div className="flex items-center py-1">
+                    <div className="flex items-center md:pt-2 md:py-0 py-3 pt-0">
                         <FaFax className="text-lg" />
                         {fax}
                     </div>
-                    <div className="flex items-center py-1">
+                    <div className="flex items-center md:pt-2 md:py-0 py-3 pt-0">
                         <MdOutlineEmail className="text-xl" />
                         {email}
                     </div>

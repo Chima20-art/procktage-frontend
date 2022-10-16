0faa7edb6a407e9e08aa013682c06d84ad57cdcc
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-16 10:50:10 +0100

    fixed contact map.

--- pages/contact.js
+++ pages/contact.js
@@ -9,19 +9,22 @@ const AnyReactComponent = ({ text }) => (
         <div>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
+                width="48"
+                height="48"
                 viewBox="0 0 24 24"
-                fill="black"
-                className="w-6 h-6"
+                fill="none"
+                stroke="red"
+                strokeWidth="2.5"
+                strokeLinecap="round"
+                strokeLinejoin="arcs"
             >
-                <path
-                    fillRule="evenodd"
-                    d="M7.5 5.25a3 3 0 013-3h3a3 3 0 013 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0112 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 017.5 5.455V5.25zm7.5 0v.09a49.488 49.488 0 00-6 0v-.09a1.5 1.5 0 011.5-1.5h3a1.5 1.5 0 011.5 1.5zm-3 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                    clipRule="evenodd"
-                />
-                <path d="M3 18.4v-2.796a4.3 4.3 0 00.713.31A26.226 26.226 0 0012 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 01-6.477-.427C4.047 21.128 3 19.852 3 18.4z" />
+                <circle cx="12" cy="10" r="3" />
+                <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
             </svg>
         </div>
-        <h1 className="text-lg font-bold">{text}</h1>
+        <h1 className="text-md   text-red-500 items-center flex font-bold">
+            {text}
+        </h1>
     </div>
 )
 
@@ -32,10 +35,10 @@ export default function Contact({ websiteSettings, categories }) {
     console.log('categories', categories)
     const defaultProps = {
         center: {
-            lat: 10.99835602,
-            lng: 77.01502627,
+            lat: 31.67582375056415,
+            lng: -8.032326433365405,
         },
-        zoom: 14,
+        zoom: 12.5,
     }
 
     const onSubmit = async (e) => {
@@ -74,7 +77,7 @@ export default function Contact({ websiteSettings, categories }) {
                             <AnyReactComponent
                                 lat={defaultProps.center.lat}
                                 lng={defaultProps.center.lng}
-                                text="My Marker"
+                                text="Procktage"
                             />
                         </GoogleMapReact>
                     </div>
@@ -109,7 +112,6 @@ export default function Contact({ websiteSettings, categories }) {
                                 onChange={(e) => setName(e.target.value)}
                                 className="w-full bg-transparent uppercase text-[14px] text-gray outline-none h-full"
                                 placeholder="Nom de l'entreprise.."
-                                type="text"
                             />
                         </div>
                         <div className="flex bg-orange-100 h-[54px] rounded-[50px] items-center px-4  drop-shadow-xl">
@@ -131,6 +133,7 @@ export default function Contact({ websiteSettings, categories }) {
                             <input
                                 className="w-full bg-transparent uppercase text-[14px] text-gray outline-none h-full"
                                 placeholder="Nom du responsable.. "
+                                reauired
                             />
                         </div>
                         <div className="flex bg-orange-100 h-[54px] rounded-[50px] items-center px-4 drop-shadow-xl">
@@ -147,9 +150,9 @@ export default function Contact({ websiteSettings, categories }) {
                                 </svg>
                             </div>{' '}
                             <input
-                                className="w-full bg-transparent uppercase text-[14px] text-gray outline-none h-full"
-                                placeholder="Email.. "
                                 type="email"
+                                className="w-full bg-transparent uppercase text-[14px] text-gray focus:outline-none outline-none border-0 h-full"
+                                placeholder="Email.. "
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 required

fd2955112230c17535b24ff6df9208c374d9a583
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-20 21:20:26 +0100

    context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import App from 'next/app'
 import { client } from '../lib/sanity'
+import { OrdersProvider } from '../OrdersContext'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <OrdersProvider>
+            <Component {...pageProps} />
+        </OrdersProvider>
+    )
 }
 
 export default MyApp

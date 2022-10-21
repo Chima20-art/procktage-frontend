b7bf72d043a56b97fd402eac4838e162c2073510
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-21 20:23:33 +0100

    added Alert for product button.

--- pages/products/[...slug].js
+++ pages/products/[...slug].js
@@ -11,9 +11,13 @@ import { uid } from 'uid'
 export default function Product({ product, categories, websiteSettings }) {
     const { addToCart } = useContext(OrdersContext)
     const [count, setCount] = useState(0)
-
-    console.log('categories', categories)
-    console.log('product', product)
+    const [isAlertVisible, setIsAlertVisible] = useState(false)
+    const handleButtonClick = () => {
+        setIsAlertVisible(true)
+        setTimeout(() => {
+            setIsAlertVisible(false)
+        }, 800)
+    }
 
     let category = categories?.filter((item) => {
         let isCurrnetCategory = false
@@ -224,13 +228,36 @@ export default function Product({ product, categories, websiteSettings }) {
                                     +
                                 </div>
                             </div>
-                            <button
-                                onClick={onAddToCart}
-                                className=" mt-6 bg-red-700 flex w-fit items-center cursor-pointer py-[18px] px-[25px] rounded-[50px] text-[12px] text-white hover:bg-gray"
-                            >
-                                <RiShoppingCart2Fill className=" mx-2 text-xl" />{' '}
-                                ajouter au panier
-                            </button>
+                            <div className="flex items-center mt-6 gap-4">
+                                <button
+                                    onClick={() => {
+                                        onAddToCart()
+                                        handleButtonClick()
+                                    }}
+                                    className="  bg-red-700 flex w-fit items-center cursor-pointer py-[18px] px-[25px] rounded-[50px] text-[12px] text-white hover:bg-gray"
+                                >
+                                    <RiShoppingCart2Fill className=" mx-2 text-xl" />{' '}
+                                    ajouter au panier
+                                </button>
+                                {isAlertVisible && (
+                                    <div className="flex text-[10px] items-center w-fit text-black">
+                                        {' '}
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            fill="currentColor"
+                                            className="w-5 h-5"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                        <p>le produit a ete ajoute au panier</p>
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>

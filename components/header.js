1ee572143b5e2005fcdf8477865b40a8e25ee14b
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-25 14:21:29 +0100

    added mediaQueries for header.

--- components/header.js
+++ components/header.js
@@ -25,7 +25,7 @@ export default function Header({ websiteSettings, categories }) {
                         setOpenCard(false)
                     }}
                     className="w-screen h-screen absolute z-20 top-0 left-0  bg-black bg-opacity-30"
-                ></div>
+                />
             )}
             <div className=" font-raleway h-fit pt-2 pb-4 grid content-around  border border-b-red-700">
                 <div className=" px-2 text-gray  h-28 w-screen lg:max-w-5xl max-w-[90%] md:max-w-[80%]  mx-auto  flex flex-row  justify-between  ">
@@ -278,7 +278,7 @@ export default function Header({ websiteSettings, categories }) {
 
             {isOpen && (
                 <div className="md:hidden   h-[calc(100vh_-_6rem)] px-6 flex flex-col z-1000">
-                    <div className="flex h-16 items-center  ">
+                    <div className="hidden flex h-16 items-center  ">
                         <FaSearch className="mr-2 text-xl flex  self-center hover:cursor-pointer hover:text-red-700 " />
 
                         <input
@@ -288,18 +288,127 @@ export default function Header({ websiteSettings, categories }) {
                         />
                     </div>
 
-                    <div className="flex  items-center  h-16  text-sm  py-2 hover:cursor-pointer hover:border-b-2 hover:border-red-700 hover:text-red-700 ">
-                        Accueil
+                    <div className="w-full mt-2 py-4  text-xs font-bold hover:cursor-pointer   hover:text-red-700 ">
+                        <Link href="/">ACCEUIL</Link>
                     </div>
-                    <div className="flex  items-center  h-16  text-sm  py-2 hover:cursor-pointer hover:border-b-2 hover:border-red-700 hover:text-red-700 ">
-                        Entreprise
+                    <div className="w-full mt-2 py-4   text-xs font-bold hover:cursor-pointer   hover:text-red-700 ">
+                        ENTREPRISE
                     </div>
-                    <div className="flex  items-center  h-16  text-sm py-2 hover:cursor-pointer hover:border-b-2 hover:border-red-700 hover:text-red-700 ">
-                        Nos produits
+                    <div
+                        className={
+                            'relative  text-xs font-bold flex flex-col   '
+                        }
+                    >
+                        <div
+                            className="w-full mt-2 py-4 flex  text-xs font-bold hover:cursor-pointer   hover:text-red-700 "
+                            onClick={() => setClicked(!clicked)}
+                        >
+                            <p>NOS PRODUITS</p>
+                            <RiArrowDropDownLine className="text-xl " />
+                        </div>
+                        <div>
+                            {' '}
+                            <AnimatePresence>
+                                {clicked && (
+                                    <motion.div
+                                        key="categoryMenu"
+                                        initial={{ y: '-20%', opacity: 0 }}
+                                        animate={{ y: 0, opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                        className=" bg-beige flex  uppercase text-gray top-[100%]  left-0 w-full px-4 py-6  "
+                                    >
+                                        <ul>
+                                            <Link href="/categories">
+                                                <p className="hover:text-red-700">
+                                                    Tous nos categories
+                                                </p>
+                                            </Link>
+
+                                            {categories?.map(
+                                                (category, index) => {
+                                                    return (
+                                                        <div
+                                                            key={category?._id}
+                                                            className=" flex flex-col"
+                                                        >
+                                                            <div
+                                                                onClick={() => {
+                                                                    if (
+                                                                        !openCategories.includes(
+                                                                            index
+                                                                        )
+                                                                    ) {
+                                                                        setOpenCategories(
+                                                                            [
+                                                                                ...openCategories,
+                                                                                index,
+                                                                            ]
+                                                                        )
+                                                                    } else {
+                                                                        const newOpenCategories =
+                                                                            openCategories.filter(
+                                                                                (
+                                                                                    item
+                                                                                ) =>
+                                                                                    item !=
+                                                                                    index
+                                                                            )
+
+                                                                        setOpenCategories(
+                                                                            newOpenCategories
+                                                                        )
+                                                                    }
+                                                                }}
+                                                                className="  justify-between items-center py-3 flex flex-row text-[10px] hover:cursor-pointer hover:text-red-700"
+                                                            >
+                                                                {
+                                                                    category?.title
+                                                                }
+                                                                <RiArrowDropDownLine className="text-xl " />
+                                                            </div>
+                                                            {openCategories.includes(
+                                                                index
+                                                            ) && (
+                                                                <div>
+                                                                    {category.subCategories?.map(
+                                                                        (
+                                                                            subcategory
+                                                                        ) => {
+                                                                            return (
+                                                                                <Link
+                                                                                    href={`/categories/${subcategory?.slug.current}`}
+                                                                                >
+                                                                                    <li
+                                                                                        key={
+                                                                                            subcategory?._id
+                                                                                        }
+                                                                                        className="text-gray py-1 text-[10px]  hover:cursor-pointer hover:text-red-700"
+                                                                                    >
+                                                                                        -{' '}
+                                                                                        {
+                                                                                            subcategory?.title
+                                                                                        }
+                                                                                    </li>
+                                                                                </Link>
+                                                                            )
+                                                                        }
+                                                                    )}
+                                                                </div>
+                                                            )}
+                                                        </div>
+                                                    )
+                                                }
+                                            )}
+                                        </ul>
+                                    </motion.div>
+                                )}
+                            </AnimatePresence>
+                        </div>
                     </div>
+
                     <Link href="/contact">
-                        <div className="flex items-center  h-16  text-sm py-2 hover:cursor-pointer hover:border-b-2 hover:border-red-700 hover:text-red-700 ">
-                            Contact
+                        <div className="w-full mt-2 py-4   text-xs font-bold hover:cursor-pointer   hover:text-red-700 ">
+                            CONTACT
                         </div>
                     </Link>
                 </div>

e8f8c28866ec4c32cb96bef3666d0607302e25bf
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-26 13:20:24 +0100

    mediaQueries.

--- components/home.js
+++ components/home.js
@@ -8,7 +8,7 @@ export default function Accueil({ homePage }) {
     return (
         <div>
             {' '}
-            <div className="my-8 max-w-4xl mx-auto  ">
+            <div className="my-8 lg:max-w-4xl md:max-w-2xl sm:max-w-[95%] mx-auto  ">
                 {homePage?.Sections?.map((section, index) => {
                     let numberOfProducts = 0
                     section?.refrence?.subCategories?.forEach((item) => {

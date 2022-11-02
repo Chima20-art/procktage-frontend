de82507ade0fb278a37123317b7e0865b3530824
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-11-02 23:58:48 +0100

    integrated sanity data to entreprise.

--- pages/entreprise.js
+++ pages/entreprise.js
@@ -8,29 +8,22 @@ export default function Entrerise({ websiteSettings, entreprise, categories }) {
     return (
         <div className="h-full bg-[#FFF8ED] min-h-screen w-screen flex flex-col justify-between ">
             <Header websiteSettings={websiteSettings} categories={categories} />
-            <div className="lg:max-w-4xl sm:max-w-2xl  w-[95%] uppercase  text-grey-700 flex flex-col items-center mx-auto py-8 h-full   w-full border  ">
+            <div className="lg:max-w-4xl sm:max-w-2xl  w-[95%] uppercase  text-grey-700 flex flex-col items-center mx-auto py-8 h-full    ">
                 <div className=" md:max-w-[60%] max-w-[80%] my-12">
                     <img src={urlFor(entreprise?.image?.image)} />
                 </div>
-                <div className="">
-                    <p className="text-[12px] font-bold px-4 py-1">
-                        Emballage fastfood
-                    </p>
-                    <p className="text-[9px] bg-[#f5f5f5] text-justify p-4 leading-relaxed">
-                        Découvrez notre gamme d'accessoires et d'emballages
-                        adaptées à la restauration rapide - fast food. Vous
-                        trouverez tout ce dont vous avez besoin pour faire
-                        déguster et habiller les plus savoureuses préparations !
-                        Proxy propose des emballages pour le snacking à tarifs
-                        réduits et cela sans perte de qualité! Retrouvez notre
-                        gamme de gobelets, de barquettes et sacs de qualité qui
-                        garantiront une parfaite dégustation à vos clients !
-                        pots carton wraps, couverts, barquettes salade, sacs à
-                        sandwich, carrés rainés, agitateurs, il y en a pour
-                        divers usages et pour des préparations de type snacking
-                        ou vente à emporter.
-                    </p>
-                </div>
+                {entreprise?.content?.map((item) => {
+                    return (
+                        <div key={item?._key} className="my-2">
+                            <p className="text-[12px] font-bold px-4 py-1">
+                                {item?.title}
+                            </p>
+                            <p className="text-[9px] bg-red-50 text-justify p-4 leading-relaxed">
+                                {item?.text}
+                            </p>
+                        </div>
+                    )
+                })}
             </div>
             <div className="w-screen">
                 <Footer websiteSettings={websiteSettings} />

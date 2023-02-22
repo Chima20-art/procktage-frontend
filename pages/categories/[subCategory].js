67c6cb5c2552c7109c8de38a92185e929b1dfdac
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-02-22 12:56:31 +0100

    removed sizing from product description in categry page.

--- pages/categories/[subCategory].js
+++ pages/categories/[subCategory].js
@@ -95,36 +95,7 @@ export default function SubCategory({
                                                     </p>
                                                 </div>
                                             )}
-                                            {product?.description?.length >
-                                                0 && (
-                                                <div className="flex gap-2 mb-1 items-center justify-start py-2   ">
-                                                    <div className="w-6 h-full  ">
-                                                        <svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            fill="none"
-                                                            viewBox="0 0 24 24"
-                                                            strokeWidth={1.5}
-                                                            stroke="currentColor"
-                                                            className="w-6 h-6"
-                                                        >
-                                                            <path
-                                                                strokeLinecap="round"
-                                                                strokeLinejoin="round"
-                                                                d="M4.745 3A23.933 23.933 0 003 12c0 3.183.62 6.22 1.745 9M19.5 3c.967 2.78 1.5 5.817 1.5 9s-.533 6.22-1.5 9M8.25 8.885l1.444-.89a.75.75 0 011.105.402l2.402 7.206a.75.75 0 001.104.401l1.445-.889m-8.25.75l.213.09a1.687 1.687 0 002.062-.617l4.45-6.676a1.688 1.688 0 012.062-.618l.213.09"
-                                                            />
-                                                        </svg>
-                                                    </div>
 
-                                                    <p className="text-red-700 text-[10px] font-bold">
-                                                        {product?.description
-                                                            ?.map(
-                                                                (item) =>
-                                                                    item?.sizing
-                                                            )
-                                                            .join(' / ')}
-                                                    </p>
-                                                </div>
-                                            )}
                                             {product?.description?.length > 0 &&
                                                 product?.description[0]
                                                     .quantite && (

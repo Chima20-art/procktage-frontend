d9c57b9c74b091f4a83b40759a5bd95d25be8d1c
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-06-14 16:57:49 +0100

    fixed slug description null.

--- pages/products/[...slug].js
+++ pages/products/[...slug].js
@@ -14,7 +14,9 @@ export default function Product({ product, categories, websiteSettings }) {
     const { addToCart } = useContext(OrdersContext)
     const [count, setCount] = useState(0)
     const [isAlertVisible, setIsAlertVisible] = useState(false)
-    const [selectedItem, setSelectedItem] = useState(product.description[0])
+    const [selectedItem, setSelectedItem] = useState(
+        product?.description?.length > 0 ? product.description[0] : {}
+    )
     const handleButtonClick = () => {
         setIsAlertVisible(true)
         setTimeout(() => {
@@ -123,7 +125,7 @@ export default function Product({ product, categories, websiteSettings }) {
                                 )}
                             </div>
 
-                            {product.description.length > 3 ? (
+                            {product?.description?.length > 3 ? (
                                 <div className="flex flex-row ">
                                     <Slider3
                                         product={product}
@@ -165,101 +167,110 @@ export default function Product({ product, categories, websiteSettings }) {
                             <p className="text-[12px] pt-2 pb-4 ">
                                 {product?.Subcategory?.title}
                             </p>
-                            <p className=" text-[12px] py-1">
-                                selectionnez la taille:
-                            </p>
-                            <div>
-                                <div className="bg-red-700 flex fex-row py-3 ">
-                                    <div className="w-6 h-6 flex-1 flex justify-center  ">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            strokeWidth={1.5}
-                                            stroke="black"
-                                            className="w-6 h-6"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                                            />
-                                        </svg>
-                                    </div>
-                                    <div className="w-6 h-6 flex-1 flex justify-center ">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            strokeWidth={1.5}
-                                            stroke="black"
-                                            className="w-6 h-6"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M4.745 3A23.933 23.933 0 003 12c0 3.183.62 6.22 1.745 9M19.5 3c.967 2.78 1.5 5.817 1.5 9s-.533 6.22-1.5 9M8.25 8.885l1.444-.89a.75.75 0 011.105.402l2.402 7.206a.75.75 0 001.104.401l1.445-.889m-8.25.75l.213.09a1.687 1.687 0 002.062-.617l4.45-6.676a1.688 1.688 0 012.062-.618l.213.09"
-                                            />
-                                        </svg>
-                                    </div>
-                                    <div className="w-6 h-6 flex-1  flex justify-center ">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            strokeWidth={1.5}
-                                            stroke="black"
-                                            className="w-6 h-6"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
-                                            />
-                                        </svg>
-                                    </div>
-                                </div>
-                                {product?.description?.map((item, index) => {
-                                    return (
-                                        <div
-                                            key={item?._key}
-                                            onClick={() => {
-                                                setSelectedItem(item)
-                                                setIsSelected(item)
-                                            }}
-                                            className={` flex fex-row py-2 cursor-pointer text-[12px] border-b  border-red-200 relative  ${
-                                                isSelected?._key == item._key
-                                                    ? 'bg-gray static text-white  '
-                                                    : 'bg-grey-50 text-red-500 '
-                                            }`}
-                                        >
-                                            <div
-                                                className={` ${
-                                                    isSelected?._key ==
-                                                        item._key &&
-                                                    'border-t-[10px] border-t-transparent border-l-[15px] -left-[6px] border-l-red-600 border-b-[10px] border-b-transparent absolute'
-                                                }   `}
-                                            ></div>
-                                            <div className="flex-1 flex font-bold justify-center ">
-                                                {item?.reference}
+                            {product?.description?.length > 0 && (
+                                <>
+                                    <p className=" text-[12px] py-1">
+                                        selectionnez la taille:
+                                    </p>
+                                    <div>
+                                        <div className="bg-red-700 flex fex-row py-3 ">
+                                            <div className="w-6 h-6 flex-1 flex justify-center  ">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth={1.5}
+                                                    stroke="black"
+                                                    className="w-6 h-6"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                                                    />
+                                                </svg>
                                             </div>
-                                            <div className="flex-1 flex lowercase justify-center">
-                                                {item?.sizing}
+                                            <div className="w-6 h-6 flex-1 flex justify-center ">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth={1.5}
+                                                    stroke="black"
+                                                    className="w-6 h-6"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M4.745 3A23.933 23.933 0 003 12c0 3.183.62 6.22 1.745 9M19.5 3c.967 2.78 1.5 5.817 1.5 9s-.533 6.22-1.5 9M8.25 8.885l1.444-.89a.75.75 0 011.105.402l2.402 7.206a.75.75 0 001.104.401l1.445-.889m-8.25.75l.213.09a1.687 1.687 0 002.062-.617l4.45-6.676a1.688 1.688 0 012.062-.618l.213.09"
+                                                    />
+                                                </svg>
                                             </div>
-                                            <div
-                                                className={`flex-1 flex  font-bold justify-center ${
-                                                    isSelected?._key ==
-                                                    item._key
-                                                        ? 'text-white'
-                                                        : 'text-black'
-                                                } `}
-                                            >
-                                                {item?.quantite}
+                                            <div className="w-6 h-6 flex-1  flex justify-center ">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth={1.5}
+                                                    stroke="black"
+                                                    className="w-6 h-6"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                                                    />
+                                                </svg>
                                             </div>
                                         </div>
-                                    )
-                                })}
-                            </div>
+                                        {product?.description?.map(
+                                            (item, index) => {
+                                                return (
+                                                    <div
+                                                        key={item?._key}
+                                                        onClick={() => {
+                                                            setSelectedItem(
+                                                                item
+                                                            )
+                                                            setIsSelected(item)
+                                                        }}
+                                                        className={` flex fex-row py-2 cursor-pointer text-[12px] border-b  border-red-200 relative  ${
+                                                            isSelected?._key ==
+                                                            item._key
+                                                                ? 'bg-gray static text-white  '
+                                                                : 'bg-grey-50 text-red-500 '
+                                                        }`}
+                                                    >
+                                                        <div
+                                                            className={` ${
+                                                                isSelected?._key ==
+                                                                    item._key &&
+                                                                'border-t-[10px] border-t-transparent border-l-[15px] -left-[6px] border-l-red-600 border-b-[10px] border-b-transparent absolute'
+                                                            }   `}
+                                                        ></div>
+                                                        <div className="flex-1 flex font-bold justify-center ">
+                                                            {item?.reference}
+                                                        </div>
+                                                        <div className="flex-1 flex lowercase justify-center">
+                                                            {item?.sizing}
+                                                        </div>
+                                                        <div
+                                                            className={`flex-1 flex  font-bold justify-center ${
+                                                                isSelected?._key ==
+                                                                item._key
+                                                                    ? 'text-white'
+                                                                    : 'text-black'
+                                                            } `}
+                                                        >
+                                                            {item?.quantite}
+                                                        </div>
+                                                    </div>
+                                                )
+                                            }
+                                        )}
+                                    </div>
+                                </>
+                            )}
 
                             <div className=" text-[11px] mt-6 md:w-full w-fit mx-auto max-sm:text-center">
                                 Quantite (Pqt):minimum pqt{' '}

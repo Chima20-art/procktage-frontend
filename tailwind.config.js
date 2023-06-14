e509cdd69d98437bb65859e54ae1b29b8eebd22a
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-06-14 16:39:30 +0100

    added insta catalogue.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
+const Myclass = plugin(function ({ addUtilities }) {
+    addUtilities({
+        '.my-rotate-y-180': {
+            transform: 'rotateY(180deg)',
+        },
+        '.preserve-3d': {
+            transformStyle: 'preserve-3d',
+        },
+        '.perspective': {
+            perspective: '1000px',
+        },
+        '.backface-hidden': {
+            backfaceVisibility: 'hidden',
+        },
+    })
+})
 module.exports = {
+    mode: 'jit',
     content: [
         './node_modules/flowbite-react/**/*.js',
         './pages/**/*.{js,ts,jsx,tsx}',
@@ -53,5 +73,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require('flowbite/plugin')],
+    plugins: [Myclass],
 }
